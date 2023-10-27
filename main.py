0422d94764f533786a8c16278b53741a7ce5ebbf
roipicsou <pl613330@gmail.com>
2023-10-27 21:02:44 +0200

    recherche des cordonée du bloc posée et du player

--- main.py
+++ main.py
@@ -80,11 +80,61 @@ class Voxel(Button):
     def input(self, key):
         if self.hovered:
             if key == 'left mouse down':
+                positions_bloc = get_postions(str(self.get_position()), "lol")
+                print(f"le position du bloc {positions_bloc}")
+                positions_player = get_postions(str(player.get_position()), "player")
+                print(f"le position du player {positions_player}")
                 Voxel(position=self.position + mouse.normal, texture=blocks[block_id])
             elif key == 'right mouse down':
                 destroy(self)
 
-def structure(x_point, z_point, y_point, name) :
+
+def get_postions(donée, demande) :
+    positions = donée
+    positions = positions.replace("LPoint3f(", "")
+    positions = positions.replace(")","")
+    x = 0
+    y = 0
+    z = 0
+    print(positions)
+    list_postions = []
+    for i in positions :
+        if i == "," or "" :
+            pass
+        else :
+            list_postions.append(i)
+    if demande == "player" :
+        print("demande player")
+        for i in list_postions :
+            if i == "." :
+                id = list_postions.index(i)
+                print(list_postions[id - 1])
+                x = list_postions[id - 1]
+                list_postions.remove(i)
+                break
+        for i in list_postions :
+            if i == "." :
+                id = list_postions.index(i)
+                y = list_postions[id - 1]
+                print(list_postions[id - 1])
+                list_postions.remove(i)
+                break
+        for i in list_postions :
+            if i == "." :
+                id = list_postions.index(i)
+                z = list_postions[id - 1]
+                print(list_postions[id - 1])
+                list_postions.remove(i)
+                break
+    else :
+        print(list_postions)
+        x = list_postions[0]
+        y =list_postions[2]
+        z = list_postions[4]
+    return [x,y,z]
+
+
+def structure(x_point, y_point, z_point, name) :
     if name in data :
         print(f"pour le structure {name} il y a :")
         structure = data[name]
@@ -148,7 +198,7 @@ for z in range(max_platofrme):
             voxel = Voxel(position=(x, 1, z),texture=random.choice(blocks_pose))
 
 structure(-1,0,0,"hause")
-structure(-1,0,1,"404")
+structure(-1,1,0,"404")
 
 player = FirstPersonController()
 
@@ -157,5 +207,6 @@ player.jump_up_duration = jump_duration
 player.mouse_sensitivity = mouse_sensitivity
 player.speed = run_speed
 player.gravity = gravity_scale
+
 
 app.run()
